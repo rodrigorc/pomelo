a36a4d4a855af371bfd3fbdc2750c3611e1b3c49
Rodrigo Rivas Costa <rodrigorivascosta@gmail.com>
2019-05-12 19:09:01 +0200

    Add new test error1

---
+++ tests/error1.rs
@@ -0,0 +1,34 @@
+use pomelo::*;
+
+pomelo! {
+    %extra_argument String;
+
+    start ::= lines;
+    lines ::= line Eol;
+    lines ::= lines line Eol;
+
+    line ::= One Two Three { extra.push('1'); }
+    line ::= error { extra.push('0'); }
+}
+
+#[test]
+fn error() -> Result<(), String> {
+    use parser::*;
+    use Token::*;
+
+    let mut p = Parser::new(String::new(), SimpleCallback);
+
+    for t in vec![
+        One, Two, Three, Eol,
+        Two, Eol,
+        One, Two, Three, Eol,
+        One, One, Eol,
+        One, Two, Three, Eol,
+    ] {
+        p.parse(t)?;
+    }
+    assert_eq!(p.extra(), "10101");
+    //no EOI here, the parser never ends
+
+    Ok(())
+}
